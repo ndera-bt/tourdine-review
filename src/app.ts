a6867a774b221d2427f14cd2b674550cccab084d
ndera-bt <nderababa@gmail.com>
2022-11-03 15:29:31 +0100

    initial commit

--- src/app.ts
+++ src/app.ts
@@ -1,43 +1,16 @@
 import express from "express";
 import { postgres } from "./config/database";
-import multer from "multer";
 import bodyParser from "body-parser";
-import path from "path";
 import { AuthRoute } from "./routes/authRoute";
+import * as dotenv from "dotenv";
+dotenv.config({ path: ".env" });
 
 const app = express();
 
-// const fileStorage = multer.diskStorage({
-//   destination: (req: any, file, cb) => {
-//     cb(null, "images");
-//   },
-//   filename: (req: any, file, cb) => {
-//     cb(
-//       null,
-//       new Date().toISOString().replace(/:/g, "-") + "-" + file.originalname
-//     );
-//   },
-// });
-
-// const fileFilter = (req: any, file: any, cb: Function) => {
-//   if (
-//     file.mimetype === "image/png" ||
-//     file.mimetype === "image/jpg" ||
-//     file.mimetype === "image/jpeg"
-//   ) {
-//     cb(null, true);
-//   } else {
-//     cb(null, false);
-//   }
-// };
-
-// app.use(
-//   multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
-// );
+const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-//app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use(AuthRoute);
 
@@ -46,7 +19,7 @@ postgres
   .then((data) => {
     if (data.isInitialized) {
       console.log("Database connected");
-      app.listen(3000, () => {
+      app.listen(port, () => {
         console.log("Running on port 5000");
       });
     }
